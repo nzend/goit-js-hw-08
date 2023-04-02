8172bd23416e75d039af22ee9be952961f226705
nzend <115723110+nzend@users.noreply.github.com>
2023-04-02 11:23:46 +0000

    Deploying to gh-pages from @ nzend/goit-js-hw-08@283c3112d4dd670a9bcab4ea1d2631fe6752c7ea 🚀

---
+++ localstorage.30862148.js
@@ -0,0 +1,2 @@
+console.log(localStorage),localStorage.setItem("my-data",JSON.stringify({user:"Mango",age:5}));var savedData=localStorage.getItem("my-data");console.log("Data",savedData);var parsedData=JSON.parse(savedData);console.log("Parsed Data",parsedData);
+//# sourceMappingURL=localstorage.30862148.js.map
